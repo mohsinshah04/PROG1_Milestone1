9de57fbc3306db14cd2d8ae6ee983c778fb0d4f3
mohsinshah04 <shahmohsin4882@gmail.com>
2024-09-20 15:08:25 -0500

    example from class

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,28 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it
         //hi
         //again
+        int opcode = 8; // 0000 0000 0000 0000 0000 0000 0000 1000
+        // 0010 00xx xxxx xxxx xxxx xxxx xxxx xxxx
+
+        int rs = 29;// 0000 0000 0000 0000 0000 0000 0001 1101
+        // xxxx xx11 101x xxxx xxxx xxxx xxxx xxxx
+
+        int rt = 29;// 0000 0000 0000 0000 0000 0000 0001 1101
+        // xxxx xxxx xxx1 1101 xxxx xxxx xxxx xxxx
+
+        int imm = 4;// 0000 0000 0000 0000 0000 0000 0000 0100
+        // xxxx xxxx xxxx xxxx 0000 0000 0001 1101
+
+        int inst = 0;   // 0000 0000 0000 0000 0000 0000 0000 0000
+        // 0000 0000 0000 0000 0000 0000 0000 0100
+        // 0000 0000 0000 0000 0000 0000 0001 1101
+        // 0000 0000 0000 0000 0000 0000 0001 1101
+        // 0000 0000 0000 0000 0000 0000 0000 1000
+
+        inst = inst | (imm << 0);
+        inst = inst | (rt << 16);
+        inst = inst | (rs << 21);
+        inst = inst | (opcode << 26);
+        System.out.println(String.format("%08x", inst));
     }
 }
